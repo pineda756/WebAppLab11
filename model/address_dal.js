c244c3c9dbd91e7d304a5866501baf53b4973066
pineda756 <aaronpineda014@gmail.com>
2016-12-16 18:34:39 -0800

    Final
    Final

---
+++ model/address_dal.js
@@ -0,0 +1,47 @@
+var mysql   = require('mysql');
+var db  = require('./db_connection.js');
+
+/* DATABASE CONFIGURATION */
+var connection = mysql.createConnection(db.config);
+
+exports.getAll = function(callback) {
+    var query = 'SELECT * FROM address;';
+
+    connection.query(query, function(err, result) {
+        callback(err, result);
+    });
+};
+
+
+
+exports.getById = function(address_id, callback) {
+    var query = 'SELECT * FROM address WHERE address_id = ?';
+    var queryData = [address_id];
+
+    connection.query(query, queryData, function(err, result) {
+        callback(err, result);
+    });
+};
+
+exports.insert = function(params, callback) {
+    var query = 'INSERT INTO address (address_id, street, zip_code) VALUES (?, ?, ?)';
+
+    // the question marks in the sql query above will be replaced by the values of the
+    // the data in queryData
+    var queryData = [params.address_id, params.street, params.zip_code];
+
+    connection.query(query, queryData, function(err, result) {
+        callback(err, result);
+    });1
+
+}
+
+exports.delete = function(address_id, callback) {
+    var query = 'DELETE FROM address WHERE address_id = ?';
+    var queryData = [address_id];
+
+    connection.query(query, queryData, function(err, result) {
+        callback(err, result);
+    });
+
+};
---
+++ routes/company_routes.js
@@ -0,0 +1,87 @@
+var express = require('express');
+var router = express.Router();
+var company_dal = require('../model/company_dal');
+
+
+// View All companies
+router.get('/all', function(req, res) {
+    company_dal.getAll(function(err, result){
+        if(err) {
+            res.send(err);
+        }
+        else {
+            res.render('company/companyViewAll', { 'result':result });
+        }
+    });
+
+});
+
+// View the company for the given id
+router.get('/', function(req, res){
+    if(req.query.company_id == null) {
+        res.send('company_id is null');
+    }
+    else {
+        company_dal.getById(req.query.company_id, function(err,result) {
+            if (err) {
+                res.send(err);
+            }
+            else {
+                res.render('company/companyViewById', {'result': result});
+            }
+        });
+    }
+});
+
+// Return the add a new company form
+router.get('/add', function(req, res){
+    // passing all the query parameters (req.query) to the insert function instead of each individually
+    company_dal.getAll(function(err,result) {
+        if (err) {
+            res.send(err);
+        }
+        else {
+            res.render('company/companyAdd', {'company': result});
+        }
+    });
+});
+
+// insert a company record
+router.get('/insert', function(req, res){
+    // simple validation
+    if(req.query.company_name == null) {
+        res.send('Company Name must be provided.');
+    }
+    else {
+        // passing all the query parameters (req.query) to the insert function instead of each individually
+        company_dal.insert(req.query, function(err,result) {
+            if (err) {
+                res.send(err);
+            }
+            else {
+                //poor practice, but we will handle it differently once we start using Ajax
+                res.redirect(302, '/company/all');
+            }
+        });
+    }
+});
+
+// Delete a company for the given company_id
+router.get('/delete', function(req, res){
+    if(req.query.company_id == null) {
+        res.send('company_id is null');
+    }
+    else {
+        company_dal.delete(req.query.company_id, function(err, result){
+            if(err) {
+                res.send(err);
+            }
+            else {
+                //poor practice, but we will handle it differently once we start using Ajax
+                res.redirect(302, '/company/all');
+            }
+        });
+    }
+});
+
+module.exports = router;
